5d2350b2b31fae864903ae92d690d4607da99039
Felipe Pessoa <felipessoaf@hotmail.com>
2019-11-04 23:23:12 -0300

    [wip] move

--- Lua testes/shooter/mainOld.lua
+++ Lua testes/shooter/mainOld.lua
@@ -1,15 +1,3 @@
-local rx = require 'rx'
-require 'rx-love'
-
--- Maps keys to players and directions
-local keyMap = {
-  a = {-1},
-  d = {1},
-  left = {-1},
-  right = {1}
-}
-
--- Declare initial state of game
 function love.load(arg)
   if arg and arg[#arg] == "-debug" then require("mobdebug").start() end
   hero = {} -- new table for the hero
@@ -31,37 +19,21 @@ function love.load(arg)
   end
 end
 
--- Helper functions
-local function identity(...) return ... end
-local function const(val) return function() return val end end
-local function dt() return love.update:getValue() end
-local function move(dt, direction)
-  print("asdasd")
-  hero.x = hero.x + hero.speed*dt*direction
-end
-
--- Respond to key presses to move players
--- keyboard actions for our hero
-for _, key in pairs({'a', 'd', 'left', 'right'}) do
-  print(key)
-  love.update
-    :filter(function()
-      return love.keyboard.isDown(key)
-    end)
-    :map(function(dt)
-      print(key)
-      return dt, unpack(keyMap[key])
-    end)
-    :subscribe(move)
-end
-
-love.keypressed
-  :filter(function(key) return key == 'space' end)
-  :subscribe(function()
+function love.keyreleased(key)
+  -- in v0.9.2 and earlier space is represented by the actual space character ' ', so check for both
+  if (key == " " or key == "space") then
     shoot()
-  end)
+  end
+end
 
 function love.update(dt)
+  -- keyboard actions for our hero
+  if love.keyboard.isDown("left") then
+    hero.x = hero.x - hero.speed*dt
+  elseif love.keyboard.isDown("right") then
+    hero.x = hero.x + hero.speed*dt
+  end
+
   local remEnemy = {}
   local remShot = {}
 
@@ -124,26 +96,3 @@ function love.draw()
   for i,v in ipairs(hero.shots) do
     love.graphics.rectangle("fill", v.x, v.y, 2, 5)
   end
-
-  -- let's draw our enemies
-  love.graphics.setColor(0,255,255,255)
-  for i,v in ipairs(enemies) do
-    love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
-  end
-end
-
-function shoot()
-  if #hero.shots >= 5 then return end
-  local shot = {}
-  shot.x = hero.x+hero.width/2
-  shot.y = hero.y
-  table.insert(hero.shots, shot)
-end
-
--- Collision detection function.
--- Checks if a and b overlap.
--- w and h mean width and height.
-function CheckCollision(ax1,ay1,aw,ah, bx1,by1,bw,bh)
-  local ax2,ay2,bx2,by2 = ax1 + aw, ay1 + ah, bx1 + bw, by1 + bh
-  return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
-end
