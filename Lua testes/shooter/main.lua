457d0fc38054952a1a44c9a00bdf9af3e7d42f3c
Felipe Pessoa <felipessoaf@hotmail.com>
2019-11-05 23:21:29 -0300

    [wip] shooter em rx

--- Lua testes/shooter/main.lua
+++ Lua testes/shooter/main.lua
@@ -11,146 +11,142 @@ local keyMap = {
 
 -- Declare initial state of game
 love.load:subscribe(function (arg)
-  -- if arg and arg[#arg] == "-debug" then require("mobdebug").start() end
-  hero = {} -- new table for the hero
-  hero.x = 300 -- x,y coordinates of the hero
-  hero.y = 450
-  hero.width = 30
-  hero.height = 15
-  hero.speed = 150
-  hero.shots = {} -- holds our fired shots
-  hero.shotsToFire = {} -- holds our shots to fire
+    hero = {} -- new table for the hero
+    hero.x = 300 -- x,y coordinates of the hero
+    hero.y = 450
+    hero.width = 30
+    hero.height = 15
+    hero.speed = 150
+    hero.shots = {} -- holds our shots
 
---   rx.Observable.fromRange(1, 5)
---     :subscribe(function ()
---       local shot = {}
---       shot.x = 0
---       shot.y = 0
---       table.insert(hero.shotsToFire, shot)    
---     end)
+    rx.Observable.fromRange(1, 5)
+        :subscribe(function ()
+        local shot = {}
+        shot.x = 0
+        shot.y = 0
+        shot.width = 2
+        shot.height = 5
+        shot.fired = true
+        table.insert(hero.shots, shot)    
+        end)
 
     enemies = {}
     rx.Observable.fromRange(0, 6)
-    :subscribe(function ()
+    :subscribe(function (i)
         local enemy = {}
         enemy.width = 40
         enemy.height = 20
         enemy.x = i * (enemy.width + 60) + 100
         enemy.y = enemy.height + 100
+        enemy.alive = true
         table.insert(enemies, enemy)  
     end)
-
---   for i=0,7 do
---     local enemy = {}
---     enemy.width = 40
---     enemy.height = 20
---     enemy.x = i * (enemy.width + 60) + 100
---     enemy.y = enemy.height + 100
---     table.insert(enemies, enemy)
---   end
 end)
 
 -- Helper functions
 local function move(dt, direction)
-  hero.x = hero.x + hero.speed*dt*direction
+    hero.x = hero.x + hero.speed*dt*direction
 end
 
 -- Respond to key presses to move players
 -- keyboard actions for our hero
 for _, key in pairs({'a', 'd', 'left', 'right'}) do
-  love.update
-    :filter(function()
-      return love.keyboard.isDown(key)
-    end)
-    :map(function(dt)
-      return dt, unpack(keyMap[key])
-    end)
-    :subscribe(move)
+    love.update
+        :filter(function()
+            return love.keyboard.isDown(key)
+        end)
+        :map(function(dt)
+            return dt, unpack(keyMap[key])
+        end)
+        :subscribe(move)
 end
 
 love.keypressed
-  :filter(function(key) return key == 'space' end)
-  :subscribe(function()
-    shoot()
-  end)
+    :filter(function(key) return key == 'space' end)
+    :subscribe(function()
+        shoot()
+    end)
 
 love.update:subscribe(function (dt)
-  local remEnemy = {}
-  local remShot = {}
+    -- update the shots
+    for i,shot in ipairs(hero.shots) do
+        if shot.fired then
+            -- move them up up up
+            shot.y = shot.y - dt * 100
+        end
 
-  -- update the shots
-  for i,v in ipairs(hero.shots) do
-    -- move them up up up
-    v.y = v.y - dt * 100
+        -- mark shots that are not visible
+        if shot.y < 0 then
+            shot.fired = false
+        end
 
-    -- mark shots that are not visible for removal
-    if v.y < 0 then
-      table.insert(remShot, i)
+        -- check for collision with enemies
+        for ii,enemy in ipairs(enemies) do
+            if enemy.alive then
+                if CheckCollision(shot.x,shot.y,shot.width,shot.height,enemy.x,enemy.y,enemy.width,enemy.height) then
+                    -- mark that enemy dead
+                    enemy.alive = false
+                    -- mark the shot not visible
+                    shot.fired = false
+                end
+            end
+        end
     end
 
-    -- check for collision with enemies
-    for ii,vv in ipairs(enemies) do
-      if CheckCollision(v.x,v.y,2,5,vv.x,vv.y,vv.width,vv.height) then
-        -- mark that enemy for removal
-        table.insert(remEnemy, ii)
-        -- mark the shot to be removed
-        table.insert(remShot, i)
-      end
+    -- update those evil enemies
+    for i,enemy in ipairs(enemies) do
+        if enemy.alive then
+            -- let them fall down slowly
+            enemy.y = enemy.y + dt
+
+            -- check for collision with ground
+            if enemy.y > 465 then
+                -- you loose!!!
+                love.load()
+            end
+        end
     end
-  end
-
-  -- remove the marked enemies
-  for i,v in ipairs(remEnemy) do
-    table.remove(enemies, v)
-  end
-
-  for i,v in ipairs(remShot) do
-    table.remove(hero.shots, v)
-  end
-
-  -- update those evil enemies
-  for i,v in ipairs(enemies) do
-    -- let them fall down slowly
-    v.y = v.y + dt
-
-    -- check for collision with ground
-    if v.y > 465 then
-      -- you loose!!!
-    end
-  end
 end)
 
 love.draw:subscribe(function ()
-  -- let's draw a background
-  love.graphics.setColor(255,255,255,255)
+    -- let's draw a background
+    love.graphics.setColor(255,255,255,255)
 
-  -- let's draw some ground
-  love.graphics.setColor(0,255,0,255)
-  love.graphics.rectangle("fill", 0, 465, 800, 150)
+    -- let's draw some ground
+    love.graphics.setColor(0,255,0,255)
+    love.graphics.rectangle("fill", 0, 465, 800, 150)
 
-  -- let's draw our hero
-  love.graphics.setColor(255,255,0,255)
-  love.graphics.rectangle("fill", hero.x, hero.y, hero.width, hero.height)
+    -- let's draw our hero
+    love.graphics.setColor(255,255,0,255)
+    love.graphics.rectangle("fill", hero.x, hero.y, hero.width, hero.height)
 
-  -- let's draw our heros shots
-  love.graphics.setColor(255,255,255,255)
-  for i,v in ipairs(hero.shots) do
-    love.graphics.rectangle("fill", v.x, v.y, 2, 5)
-  end
+    -- let's draw our heros shots
+    love.graphics.setColor(255,255,255,255)
+    for i,shot in ipairs(hero.shots) do
+        if shot.fired then
+            love.graphics.rectangle("fill", shot.x, shot.y, shot.width, shot.height)
+        end
+    end
 
-  -- let's draw our enemies
-  love.graphics.setColor(0,255,255,255)
-  for i,v in ipairs(enemies) do
-    love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
-  end
+    -- let's draw our enemies
+    love.graphics.setColor(0,255,255,255)
+    for i,enemy in ipairs(enemies) do
+        if enemy.alive then
+            love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
+        end
+    end
 end)
 
 function shoot()
-  if #hero.shots >= 5 then return end
-  local shot = {}
-  shot.x = hero.x+hero.width/2
-  shot.y = hero.y
-  table.insert(hero.shots, shot)
+    --filter not fired. first.
+    for i,shot in ipairs(hero.shots) do 
+        if not shot.fired then
+            shot.x = hero.x+hero.width/2
+            shot.y = hero.y
+            shot.fired = true
+            break
+        end
+    end
 end
 
 -- Collision detection function.
