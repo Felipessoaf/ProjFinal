fe8a765bfcf754a62da5c26bee6d2a2a16b33cc8
Felipe Pessoa <felipessoaf@hotmail.com>
2020-03-21 16:30:26 -0300

    map man

--- MainGame/MapManager.lua
+++ MainGame/MapManager.lua
@@ -4,6 +4,10 @@ local sti = require "sti"
 local MapManager = {}
 
 function MapManager.InitMap()
+   
+   -- the height of a meter our worlds will be 64px
+   love.physics.setMeter(64)
+
    return sti("Maps/mainMap.lua", { "box2d" })
 end
 
